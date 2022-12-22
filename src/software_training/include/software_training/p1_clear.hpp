68073f74d0f0b3af251c047d196e16deb7c563fd
edward <edawordwang@gmail.com>
2022-12-22 01:54:03 -0500

    p1 header + testing launch.py

--- src/software_training/include/software_training/p1_clear.hpp
+++ src/software_training/include/software_training/p1_clear.hpp
@@ -11,11 +11,18 @@
 
 #include <turtlesim/srv/kill.hpp>
 
-
+namespace composition {
 class p1_clear : public rclcpp::Node {
     public:
-        p1_clear();
-        int x;
+        p1_clear(const rclcpp::NodeOptions &options);
+    private:
+        std::shared_ptr<rclcpp::Client<turtlesim::srv::Kill>> client;
+        std::vector<std::string> nodes = {"turtle1"}; // why static?
+        std::shared_ptr<rclcpp::TimerBase> timer_;
+        void kill();
+
 };
+
+}
 
 // #endif
--- src/software_training/src/p1_clear.cpp
+++ src/software_training/src/p1_clear.cpp
@@ -1,10 +1,21 @@
 #include "../include/software_training/p1_clear.hpp"
+#include <iostream>
 
-p1_clear::p1_clear() : Node("p1_clear"){
+using namespace composition;
+
+p1_clear::p1_clear(const rclcpp::NodeOptions &options) : Node("p1_clear", options){
+    // why pass options?
+    client = this->create_client<turtlesim::srv::Kill>("kill");
+    // create_client<service type>(name)
+    timer_ = create_wall_timer(
+        std::chrono::duration
+            <int, std::chrono::seconds::period>(1), 
+        std::bind(&p1_clear::kill, this)
+    );
+    
     std::cout<<"whast"<<std::endl;
 }
 
-#include <iostream>
 
 // int main() {
 //     std::cout<<"HJELLOOO"<<std::endl;
