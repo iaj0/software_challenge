12133bb975b3942011256b7f1b7b945b7b36fc24
edward <edawordwang@gmail.com>
2022-12-22 20:03:05 -0500

    clean up

--- build/software_training/CMakeFiles/software_training_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/software_training_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/software_training_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
