b36df4a6fb5fca7eed8d2a80f3ee402f8f585e4e
wang-edward <edawordwang@gmail.com>
2023-01-22 11:11:20 -0500

    p6_waypoint

--- src/software_training/include/software_training/p6_waypoint.hpp
+++ src/software_training/include/software_training/p6_waypoint.hpp
@@ -21,62 +21,74 @@
 namespace composition {
 
 class p6_waypoint : public rclcpp::Node {
+
 public:
     p6_waypoint(const rclcpp::NodeOptions &options);
 
-private:
-    rclcpp_action::Server<software_training::action::Waypoint>::SharedPtr action_server;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher;
-    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber;
+    using GoalHandleActionServer = rclcpp_action::ServerGoalHandle<software_training::action::Waypoint>;
 
+private:
     rclcpp_action::GoalResponse handle_goal(
         const rclcpp_action::GoalUUID &uuid,
         std::shared_ptr<const software_training::action::Waypoint::Goal> goal
     );
+
     rclcpp_action::CancelResponse handle_cancel(
-        const std::shared_ptr
-            <rclcpp_action::ServerGoalHandle
-                <software_training::action::Waypoint>> goal_handle
+        const std::shared_ptr<GoalHandleActionServer> goal_handle
     );
 
-    void handle_accepted(const std::shared_ptr
-        <rclcpp_action::ServerGoalHandle
-            <software_training::action::Waypoint>> goal_handle
+    void handle_accepted(
+        const std::shared_ptr<GoalHandleActionServer> goal_handle
     );
 
-    void execute(const std::shared_ptr
-        <rclcpp_action::ServerGoalHandle
-            <software_training::action::Waypoint>> goal_handle
-    );
+    void execute(const std::shared_ptr<GoalHandleActionServer> goal_handle);
 
-    static float x;
-    static float y;
-    static float theta;
-    static float linear_velocity;
-    static float angular_velocity;
-    
+    rclcpp_action::Server<software_training::action::Waypoint>::SharedPtr action_server;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subscriber;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher;
+
+    double x = 0.0f;
+    double y = 0.0f;
+    double theta = 0.0f;
+    double linear_velocity = 0.0f;
+    double angular_velocity = 0.0f;
+
     template <typename T>
  
-    struct vec3 {
-        vec3(T x=0, T y=0, T z=0): x{x}, y{y}, z{z} { }
+    struct vec2 {
+        vec2(T x=0, T y=0): x{x}, y{y} { }
+        // copy constructor
+        vec2(const vec2& a) {
+            this->x = a.x;
+            this->y = a.y;
+        }
+        // converting from types
+        template <typename T2> 
+        vec2(const vec2<T2> &other) {
+            this->x = other.x;
+            this->y = other.y;
+        } 
+
+        vec2 operator+(vec2& b) {
+            return {this->x + b.x, this->y + b.y};
+        }
+
+        vec2 operator-(vec2&b) {
+            return {this->x - b.x, this->y - b.y};
+        }
+
+
+        
         T x;
         T y;
-        T z;
-        bool or_less(vec3 b) {
-            return x < b.x || y < b.y || z < b.z;
-        }
-        vec3 advance(vec3& goal) {
-            vec3 ans;
-            ans.x = (x < goal.x) ? ++x : x;
-            ans.y = (y < goal.y) ? ++y : y;
-            ans.z = (z < goal.z) ? ++z : z;
-            return ans;
+        // current is within error of goal
+        bool in_range(vec2 goal, double error) {
+            return (abs(goal.x - x) <= error) && (abs(goal.y - y) <= error);
         }
     };
 
-    typedef vec3<float> vec3f; 
-    typedef vec3<double> vec3d;
-    typedef vec3<float&> vec3f_ref;
+    typedef vec2<double> vec2d; 
+    typedef vec2<double&> vec2d_ref;
 };
 
 } // namespace composition
--- src/software_training/src/p6_waypoint.cpp
+++ src/software_training/src/p6_waypoint.cpp
@@ -4,10 +4,21 @@ using namespace std::placeholders;
 namespace composition {
 
 p6_waypoint:: p6_waypoint(const rclcpp::NodeOptions& options) : Node{"p6_waypoint", options} {
+        
     // idea: 
     // publisher to send moving data
     // subscriber to read current position
     // action server updates current pos and sends result + duration
+
+    // (post) better way wouldve been to give p6_waypoint it's own vec2s instead of making curr_pos
+
+    this->action_server = rclcpp_action::create_server<software_training::action::Waypoint>(
+        this, "p6_waypoint_action_server",
+        std::bind(&p6_waypoint::handle_goal, this, _1, _2),
+        std::bind(&p6_waypoint::handle_cancel, this, _1),
+        std::bind(&p6_waypoint::handle_accepted, this, _1)
+    );
+
     this->publisher = this->create_publisher<geometry_msgs::msg::Twist>(
         "/moving_turtle/cmd_vel", 10
     );
@@ -23,41 +34,22 @@ p6_waypoint:: p6_waypoint(const rclcpp::NodeOptions& options) : Node{"p6_waypoin
     this->subscriber = this->create_subscription<turtlesim::msg::Pose>(
         "/moving_turtle/pose", 10, subscriber_callback
     );
-
-    this->action_server = 
-        rclcpp_action::create_server<software_training::action::Waypoint>(
-            this, "p6_waypoint_action_server",
-            std::bind(&p6_waypoint::handle_goal, this, _1, _2),
-            std::bind(&p6_waypoint::handle_cancel, this, _1),
-            std::bind(&p6_waypoint::handle_accepted, this, _1)
-        );
 }
 
 rclcpp_action::GoalResponse p6_waypoint::handle_goal(
-    const rclcpp_action::GoalUUID &uuid, 
-    std::shared_ptr<const software_training::action::Waypoint::Goal> goal) {
-    
-    // print
-    std::cout<<"HANDLE_GOAL()\n";
-    std::cout<<"linear: ";
-    std::cout<<goal->linear_pos.x<<" ";
-    std::cout<<goal->linear_pos.y<<" ";
-    std::cout<<goal->linear_pos.z<<" ";
-    std::cout<<"angular: ";
-    std::cout<<goal->angular_pos.x<<" ";
-    std::cout<<goal->angular_pos.y<<" ";
-    std::cout<<goal->angular_pos.z<<" ";
-    std::cout<<std::endl;
+        const rclcpp_action::GoalUUID &uuid,
+        std::shared_ptr<const software_training::action::Waypoint::Goal> goal) {
 
+    (void)uuid;
+    // print
+    RCLCPP_INFO(this->get_logger(), "linear x: %f, y: %f", goal->x, goal->y);
     return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 }
 
 rclcpp_action::CancelResponse p6_waypoint:: handle_cancel(
-        const std::shared_ptr
-            <rclcpp_action::ServerGoalHandle
-                <software_training::action::Waypoint>> goal_handle) {
-    
-    std::cout<<"CANCEL_GOAL()"<<std::endl;
+        const std::shared_ptr<GoalHandleActionServer> goal_handle) {
+    (void) goal_handle;
+    RCLCPP_INFO(this->get_logger(), "CANCEL");
     
     return rclcpp_action::CancelResponse::ACCEPT;
 }
@@ -73,77 +65,61 @@ void p6_waypoint:: handle_accepted(const std::shared_ptr
     }.detach();
 }
 
-void p6_waypoint:: execute(const std::shared_ptr
-        <rclcpp_action::ServerGoalHandle
-            <software_training::action::Waypoint>> goal_handle) {
-
+void p6_waypoint::execute(const std::shared_ptr<GoalHandleActionServer> goal_handle) {
+    auto start_time = this->now(); 
 
     const auto goal = goal_handle->get_goal();
-    auto start_time = this->now();
-
-    rclcpp::Rate rate{1};
-
-    auto feedback = std::make_unique<software_training::action::Waypoint::Feedback>();
     auto result = std::make_unique<software_training::action::Waypoint::Result>();
 
-    p6_waypoint::vec3d cur_lin{};
-    p6_waypoint::vec3d cur_ang{}; 
+    vec2d_ref curr_pos{this->p6_waypoint::x, this->p6_waypoint::y};
+    vec2d old_pos {curr_pos};
+    vec2d goal_pos{goal->x, goal->y};
 
-    p6_waypoint::vec3d goal_lin{goal->linear_pos.x, goal->linear_pos.y, goal->linear_pos.z};
-    p6_waypoint::vec3d goal_ang{goal->angular_pos.x, goal->angular_pos.y, goal->angular_pos.z};
+    RCLCPP_INFO(this->get_logger(), "current position: (%f, %f)", curr_pos.x, curr_pos.y);
+    RCLCPP_INFO(this->get_logger(), "goal: (%f, %f)", goal_pos.x, goal_pos.y);
 
-    while (rclcpp::ok() && (cur_lin.or_less(goal_lin) || cur_ang.or_less(goal_ang))) {
+    double error = 0.05;
 
-        // cancel
-        if (goal_handle->is_canceling()) {
-            std::cout<<"GOAL_HANDLE->is_canceling()"<<std::endl;
-            auto curr_time = this->now();
-            auto time = curr_time - start_time;
-            long int duration{time.nanoseconds()};
-            result->duration = duration;
-            goal_handle->canceled(std::move(result));
-            return;
-        }
+    while(rclcpp::ok() && !(goal_pos.in_range(curr_pos, error))) {
+        // update velocity with current pos for higher accuracy
+        vec2d velocity{goal_pos.x - curr_pos.x, goal_pos.y - curr_pos.y};
+        RCLCPP_INFO(this->get_logger(), "velocity: (%f, %f)", velocity.x, velocity.y);
 
-        auto msg_cmd_vel = std::make_unique<geometry_msgs::msg::Twist>();
-        vec3d temp_lin = cur_lin.advance(goal_lin);
-        vec3d temp_ang = cur_ang.advance(goal_ang);
+        auto message = std::make_unique<geometry_msgs::msg::Twist>();
+        message->angular.x = 0.0f;
+        message->angular.y = 0.0f;
+        message->angular.z = 0.0f;
+        message->linear.x = velocity.x;
+        message->linear.y = velocity.y;
+        message->linear.z = 0.0f;
 
-        // fill out message
-        msg_cmd_vel->linear.x = temp_lin.x;
-        msg_cmd_vel->linear.y = temp_lin.y;
-        msg_cmd_vel->linear.z = temp_lin.z;
+        auto feedback = std::make_unique<software_training::action::Waypoint::Feedback>();
+        feedback->distance = sqrt(pow(goal_pos.x - curr_pos.x, 2)+ pow(goal_pos.x - curr_pos.y, 2));
 
-        msg_cmd_vel->angular.x = temp_ang.x;
-        msg_cmd_vel->angular.y = temp_ang.y;
-        msg_cmd_vel->angular.z = temp_ang.z;
-
-        // send on /moving_turtle/cmd_vel
-        this->publisher->publish(std::move(msg_cmd_vel));
-
-        // update (static x + y get updated from subscriber)
-        feedback->x_pos = this->p6_waypoint::x - cur_lin.x;
-        feedback->y_pos = this->p6_waypoint::y - cur_lin.y;
-
-        // some math for calculating angle?
-        double magnitude = static_cast<double>(sqrt(pow(cur_lin.x, 2) + pow(cur_lin.y, 2) + pow(cur_lin.z, 2)));
-        theta = acos(cur_lin.z / magnitude);
-
-        feedback->theta_pos = this->p6_waypoint::theta - theta;
-
-        // publish on feedback topic
+        this->publisher->publish(std::move(message));
         goal_handle->publish_feedback(std::move(feedback));
-        rate.sleep();
     }
-
-    // result
+    
     if (rclcpp::ok()) {
-        auto end = this->now();
-        rclcpp::Duration duration = end - start_time;
-        long int res_time{duration.nanoseconds()};
-        result->duration = res_time;
-        goal_handle->succeed(std::move(result));
-        std::cout<<"goal done"<<std::endl;
+
+        auto message = std::make_unique<geometry_msgs::msg::Twist>();
+        message->angular.x = 0.0f;
+        message->angular.y = 0.0f;
+        message->angular.z = 0.0f;
+        message->linear.x = 0.0f;
+        message->linear.y = 0.0f;
+        message->linear.z = 0.0f;
+
+        this->publisher->publish(std::move(message));
+
+        rclcpp::Time end_time = this->now();       
+        rclcpp::Duration duration = end_time - start_time; 
+
+        result->duration = duration.nanoseconds();
+
+        goal_handle->succeed(std::move(result)); 
+        RCLCPP_INFO(this->get_logger(), "DONE!!!!");
+        RCLCPP_INFO(this->get_logger(), "moved from (%f, %f) -> (%f, %f)", old_pos.x, old_pos.y, goal_pos.x, goal_pos.y);
     }
 }
 
