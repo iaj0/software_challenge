27fe939f3db0e7e070d933474d4bec6774c7b842
Iram Anwar <iram@live.com.sg>
2024-04-17 18:53:45 -0400

    p3 done

--- src/software_training/include/software_training/p3_spawn.hpp
+++ src/software_training/include/software_training/p3_spawn.hpp
@@ -1,1 +1,44 @@
+#pragma once
 
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+
+#include <turtlesim/srv/spawn.hpp>
+
+namespace composition {
+
+class p3_spawn : public rclcpp::Node {
+    public:
+        p3_spawn(const rclcpp::NodeOptions &options);
+    private:
+        // complete this code
+        rclcpp::Client<turtlesim::srv::Spawn>::SharedPtr client;
+        rclcpp::TimerBase::SharedPtr timer;
+
+        static const unsigned int NUMBER_OF_TURTLES{2};
+
+        typedef struct turtle_info {
+          float x_pos;
+          float y_pos;
+          float rad;
+        } turtle_info;
+  
+        std::vector<std::string> turtle_names{"stationary_turtle", "moving_turtle"};
+        std::vector<turtle_info> turtle_bio{{.x_pos = 5, .y_pos = 5, .rad = 0},
+                                          {.x_pos = 25, .y_pos = 10, .rad = 0}};
+  
+        // map of turtle name to turtle information
+        std::map<std::string, turtle_info> turtle_description;
+
+        std::map<std::string, turtle_info>::iterator it = turtle_description.begin();
+
+
+        void spawn_turtle();
+
+};
+
+} // namespace composition
--- src/software_training/src/p3_spawn.cpp
+++ src/software_training/src/p3_spawn.cpp
@@ -1,1 +1,52 @@
-#
+#include "../include/software_training/p3_spawn.hpp"
+#include <cstdlib>
+#include <memory>
+#include <string>
+#include <vector>
+#include <iostream>
+
+namespace composition {
+
+p3_spawn:: p3_spawn(const rclcpp::NodeOptions &options)
+    : Node{"p3_spawn", options} {
+    // complete this code
+
+    client = this->create_client<turtlesim::srv::Spawn>("spawn");
+    //map turtle name to turtle information
+    //stationary turtle
+    turtle_description[turtle_names[0]] = turtle_bio[0];
+    //moving turtle
+    turtle_description[turtle_names[1]] = turtle_bio[1];
+
+    p3_spawn::spawn_turtle();
+
+} 
+
+void p3_spawn::spawn_turtle(){
+    for(auto turtles: turtle_names){
+        auto message = std::make_shared<turtlesim::srv::Spawn::Request>();
+        message->x = turtle_description[turtles].x_pos;
+        message->y = turtle_description[turtles].y_pos;
+        message->theta = turtle_description[turtles].rad;
+        message->name = turtles;
+        std::cout<<turtle_description[turtles].x_pos<<std::endl;
+
+        auto callback = [this] (void) -> void {
+               //output to log
+            //    (void)response;
+                RCLCPP_INFO(this->get_logger(), "Spawned Turtle");
+
+               rclcpp::shutdown(); // kill this node
+       };
+        auto result = client->async_send_request(message);
+       
+    }
+    
+}
+    
+
+} // namespace composition
+
+#include <rclcpp_components/register_node_macro.hpp>
+
+RCLCPP_COMPONENTS_REGISTER_NODE(composition::p3_spawn)
